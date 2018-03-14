b391fde70bf5a1f44ea8d64f39c0f6790f268b12
robertjbeaver <robertjbeaver@gmail.com>
2018-03-14 09:24:08 -0400

    finished

--- src/TempratureConverter.java
+++ src/TempratureConverter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.NumberFormat;
 
 public class TempratureConverter {
 
@@ -6,18 +7,23 @@ public class TempratureConverter {
 		// welcome
 		System.out.println("Welcome to the Temprature Converter.\n");
 		
-		//open scanner
+		//open scanner and init NumberFormat
 		Scanner sc = new Scanner(System.in);
 		String choice = "y";
+		NumberFormat number = NumberFormat.getNumberInstance();
+		number.setMaximumFractionDigits(2);
 		
 		//user input and output
 		while(!choice.equalsIgnoreCase("n")) {
-			System.out.println("Enter degrees in Fahrenheit:  ");
-			double temp = sc.nextDouble();
-			double convertedTemp = (temp-32)*5/7; 
-			System.out.println("Degrees in Celsius:  "+(double)convertedTemp);
+			System.out.print("Enter degrees in Fahrenheit:  ");
+			double temp = sc.nextDouble();		
+			double convertedTemp = (temp-32)*5/7;			
+            String convertedTempFormat = number.format(convertedTemp); 
+			System.out.println("Degrees in Celsius:  " + convertedTempFormat);			
+			System.out.print("Continue? (y/n): ");
+            choice = sc.next();
 		}
-
+		System.out.println("Bye!");
 	}
 
 }
